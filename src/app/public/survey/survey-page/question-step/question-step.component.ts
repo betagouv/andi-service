f52131d8c7f22e9c9a49be54b0c1a01ae4b5477f
Gibraïl Berdouk <gibrail.berdouk@gmail.com>
2019-11-25 18:45:46 +0100

    Created question block for survey step by step

---
+++ src/app/public/survey/survey-page/question-step/question-step.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'andi-question-step',
+  templateUrl: './question-step.component.html',
+  styleUrls: ['./question-step.component.scss']
+})
+export class QuestionStepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
