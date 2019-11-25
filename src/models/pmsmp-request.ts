290ed6760c847e178c8c018d80a22262c2fe0264
Gibraïl Berdouk <gibrail.berdouk@gmail.com>
2019-11-25 18:43:56 +0100

    Added request and response models

---
+++ src/models/pmsmp-request.ts
@@ -0,0 +1,66 @@
+export class PmsmpRequest {
+  readonly _v: number = 1;
+  _timestamp: string;
+  _query_id: string;
+  _session_id: string;
+  address: Address;
+  criteria: Criteria[];
+
+  constructor(
+    _timestamp: string,
+    _query_id: string,
+    _session_id: string,
+    address: Address,
+    criteria: Criteria[]
+  ) {
+    this._timestamp = _timestamp;
+    this._query_id = _query_id;
+    this._session_id = _session_id;
+    this.address = address;
+    this.criteria = criteria;
+  }
+}
+
+export class Address {
+  type: ADDRESS_TYPE.string | ADDRESS_TYPE.geoapigouv;
+  value: string;
+
+  constructor(
+    type: ADDRESS_TYPE.string | ADDRESS_TYPE.geoapigouv,
+    value: string
+  ) {
+    this.type = type;
+    this.value = value;
+  }
+}
+
+export class Criteria {
+  type: CRITERIA_TYPE.distance | CRITERIA_TYPE.rome_codes;
+  value: any; // optional
+  priority: number;
+
+  constructor(
+    type: CRITERIA_TYPE.distance | CRITERIA_TYPE.rome_codes,
+    value: number | RomeCode,
+    priority: number
+  ) {
+    this.type = type;
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
+export class RomeCode {
+  code: string;
+  priority: number;
+}
+
+export enum ADDRESS_TYPE {
+  string = "string",
+  geoapigouv = "geoapigouv"
+}
+
+export enum CRITERIA_TYPE {
+  distance = "distance",
+  rome_codes = "rome_codes"
+}
---
+++ src/models/pmsmp-result.ts
@@ -0,0 +1,100 @@
+export class PmsmpResult {
+  readonly _v: number = 1;
+  _timestamp: string;
+  _query_id: string;
+  _session_id: string;
+  _trace: string;
+  data: Data[];
+
+  constructor(options: {
+    _timestamp: string;
+    _query_id: string;
+    _session_id: string;
+    _trace: string;
+    data: Data[];
+  }) {
+    this._timestamp = options._timestamp;
+    this._query_id = options._query_id;
+    this._session_id = options._session_id;
+    this._trace = options._trace;
+    this.data = options.data;
+  }
+}
+
+export class Data {
+  id: string;
+  name: string;
+  address: string;
+  departement: string;
+  city: string;
+  coords: Coords;
+  naf: string;
+  siret: string;
+  distance: number;
+  scoring: Scoring;
+  score: number;
+  activity: string;
+  size: string;
+
+  constructor(options: {
+    id: string;
+    name: string;
+    address: string;
+    departement: string;
+    city: string;
+    coords: Coords;
+    naf: string;
+    siret: string;
+    distance: number;
+    scoring: Scoring;
+    score: number;
+    activity: string;
+    size: string;
+  }) {
+    this.id = options.id;
+    this.name = options.name;
+    this.address = options.address;
+    this.departement = options.departement;
+    this.city = options.city;
+    this.coords = options.coords;
+    this.naf = options.naf;
+    this.siret = options.siret;
+    this.distance = options.distance;
+    this.scoring = options.scoring;
+    this.score = options.score;
+    this.activity = options.activity;
+    this.size = options.size;
+  }
+}
+
+export class Coords {
+  lat: number;
+  lon: number;
+
+  constructor(options: { lat: number; lon: number }) {
+    this.lat = options.lat;
+    this.lon = options.lon;
+  }
+}
+
+export class Scoring {
+  geo: number;
+  size: number;
+  contact: number;
+  pmsmp: number;
+  naf: number;
+
+  constructor(options: {
+    geo: number;
+    size: number;
+    contact: number;
+    pmsmp: number;
+    naf: number;
+  }) {
+    this.geo = options.geo;
+    this.size = options.size;
+    this.contact = options.contact;
+    this.pmsmp = options.pmsmp;
+    this.naf = options.naf;
+  }
+}
