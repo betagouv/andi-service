1927f275c42e6bd4217b42d3e40d7014a282d80f
Gibraïl Berdouk <gibrail.berdouk@gmail.com>
2019-11-26 18:34:33 +0100

    Refactor survey page and question step comp - delegate more responsability for question step

--- src/app/public/survey/survey-page/question-step/question-step.component.ts
+++ src/app/public/survey/survey-page/question-step/question-step.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ResponseProposal } from 'src/models/response-proposal.model';
 import { QuestionType } from 'src/models/question-step.model';
+import { QuestionStep } from '../../../../../models/question-step.model';
 
 @Component({
   selector: 'andi-question-step',
@@ -8,13 +9,13 @@ import { QuestionType } from 'src/models/question-step.model';
   styleUrls: ['./question-step.component.scss']
 })
 export class QuestionStepComponent implements OnInit {
-  @Input() sentence: string;
-  @Input() description: string;
-  @Input() type: QuestionType;
-  @Input() proposals: ResponseProposal[];
+
+  @Input() questionStep: QuestionStep;
 
   constructor() {}
 
   ngOnInit() {
+    console.log(this.questionStep.sentence);
+    console.log(this.questionStep.proposals);
   }
 }
