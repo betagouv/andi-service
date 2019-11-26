e624c3e00107b924f76b50c4b21f317fb99f063f
Gibraïl Berdouk <gibrail.berdouk@gmail.com>
2019-11-26 11:11:37 +0100

    Load on survey page comp unique question step for now and display it attributes

--- src/app/public/survey/survey-page/survey-page.component.ts
+++ src/app/public/survey/survey-page/survey-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { QuestionStep, QuestionType } from 'src/models/question-step.model';
 
 @Component({
   selector: 'andi-survey-page',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SurveyPageComponent implements OnInit {
 
+  questionSteps = new QuestionStep('Test', 'Toto', QuestionType.REDIRECT, undefined);
+  
   constructor() { }
 
   ngOnInit() {
