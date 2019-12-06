2cc1ab8c9e50c123d7912a56cb3ce6c8f8a30f36
Gibraïl Berdouk <gibrail.berdouk@gmail.com>
2019-12-06 12:43:00 +0100

    Disable previous button when first question step

--- src/app/public/survey/survey-page/navigation-step/navigation-step.component.ts
+++ src/app/public/survey/survey-page/navigation-step/navigation-step.component.ts
@@ -8,14 +8,18 @@ import { SurveyStepperSharedService } from 'src/app/core/services/survey-stepper
   styleUrls: ['./navigation-step.component.scss']
 })
 export class NavigationStepComponent implements OnInit {
-
+  enablePrev = false;
 
   constructor(
     private surveyStepperSharedService: SurveyStepperSharedService,
     private router: Router
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.surveyStepperSharedService.stepperCursor.subscribe(stepId => {
+      this.enablePrev = stepId === 'Q1' ? false : true;
+    });
+  }
 
   previousQuestion() {
     this.surveyStepperSharedService.goToPrevStep();
