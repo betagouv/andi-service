b5c4653d6caf99c686203b1bea2276223bb2aa08
Gibraïl Berdouk <gibrail.berdouk@gmail.com>
2020-01-31 15:50:00 +0100

    Fixed lint errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { filter, map, mergeMap } from 'rxjs/operators';
-
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { Title } from '@angular/platform-browser';
-import { Meta } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
 import * as globals from './globals';
 
 @Component({
@@ -11,34 +9,35 @@ import * as globals from './globals';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent implements OnInit {
   title = 'andi-service';
-  
+
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private titleService: Title,
     private meta: Meta
   ) {
-      const metaTag = this.meta.getTag('name=andi_id');
-      if (metaTag) {
-        globals.setSessionId(metaTag.content);
-      }
+    const metaTag = this.meta.getTag('name=andi_id');
+    if (metaTag) {
+      globals.setSessionId(metaTag.content);
     }
+  }
   /* tslint:disable:no-string-literal */
   ngOnInit() {
     this.router.events
       .pipe(
-        filter((event) => event instanceof NavigationEnd),
+        filter(event => event instanceof NavigationEnd),
         map(() => this.activatedRoute),
-        map((route) => {
-          while (route.firstChild) { route = route.firstChild; }
+        map(route => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
           return route;
         }),
-        filter((route) => route.outlet === 'primary'),
-        mergeMap((route) => route.data)
+        filter(route => route.outlet === 'primary'),
+        mergeMap(route => route.data)
       )
-      .subscribe((event) => this.titleService.setTitle(event['title']));
+      .subscribe(event => this.titleService.setTitle(event['title']));
   }
 }
