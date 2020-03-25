9b5d20ae972b26e9cedae96199def3a7e80c9e78
pmontens <pieterjan@montens.net>
2020-03-25 11:32:08 +0100

    integration tracker, test

--- src/app/public/firm/firm-page/firm-page.component.ts
+++ src/app/public/firm/firm-page/firm-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackingService } from 'src/app/core/services/tracking.service';
 
 @Component({
   selector: 'andi-firm-page',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirmPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private trackingService: TrackingService
+  ) {}
 
   ngOnInit() {
+    this.trackingService.track('employeurs', StepContext.ARRIVAL);
   }
 
 }
