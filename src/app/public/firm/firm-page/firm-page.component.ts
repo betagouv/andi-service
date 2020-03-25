460e1f83db54aa117fbe3ceecfdf239e7f8d667f
pmontens <pieterjan@montens.net>
2020-03-25 14:26:00 +0100

    tested tracking on firm page

--- src/app/public/firm/firm-page/firm-page.component.ts
+++ src/app/public/firm/firm-page/firm-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { TrackingService } from 'src/app/core/services/tracking.service';
+import { StepContext } from 'src/models/tracking-request.model';
 
 @Component({
   selector: 'andi-firm-page',
