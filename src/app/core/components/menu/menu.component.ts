167898a9e89eda7b0f5d00cdcb11b2ecffe3f56d
Gibrail Berdouk <gibrail.berdouk@gmail.com>
2020-05-04 21:02:59 +0200

    New menu

---
+++ src/app/core/components/menu/menu.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {TrackingService} from '../../services/tracking.service';
+import {StepContext} from '../../../../models/tracking-request.model';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'andi-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor(private trackingService: TrackingService,
+              private router: Router,) {
+  }
+
+  ngOnInit() {
+  }
+
+  goToHome() {
+    this.trackingService.track('header', StepContext.LINKTO, {link: 'home'});
+    window.open('https://andi.beta.gouv.fr', '_self');
+  }
+
+  goToSearch() {
+    this.router.navigateByUrl('/summary');
+  }
+
+  goToFirm() {
+    this.router.navigateByUrl('/employeurs');
+  }
+}
