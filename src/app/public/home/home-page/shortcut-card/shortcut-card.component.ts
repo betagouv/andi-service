4853d861093e3c56828a33b47fdc75828a0adef8
Gibrail Berdouk <gibrail.berdouk@gmail.com>
2020-05-06 14:42:48 +0200

    shortcut card provisory

---
+++ src/app/public/home/home-page/shortcut-card/shortcut-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'andi-shortcut-card',
+  templateUrl: './shortcut-card.component.html',
+  styleUrls: ['./shortcut-card.component.scss']
+})
+export class ShortcutCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -3,10 +3,11 @@ import {CommonModule} from '@angular/common';
 import {HomePageComponent} from './home-page/home-page.component';
 import {routes} from '../../app-routing.module';
 import {RouterModule} from '@angular/router';
+import { ShortcutCardComponent } from './home-page/shortcut-card/shortcut-card.component';
 
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, ShortcutCardComponent],
   imports: [
     CommonModule,
     RouterModule.forRoot(routes)
