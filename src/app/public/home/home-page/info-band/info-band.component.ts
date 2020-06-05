4cdbc7df97ee1bcf3da52d67eeb29177aaa99ed9
parlamente <60840523+parlamente@users.noreply.github.com>
2020-06-05 14:48:25 +0200

    ajour component info-band sur la home page

---
+++ src/app/public/home/home-page/info-band/info-band.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'andi-info-band',
+  templateUrl: './info-band.component.html',
+  styleUrls: ['./info-band.component.scss']
+})
+export class InfoBandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
